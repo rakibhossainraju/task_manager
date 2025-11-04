74ca93cdfb70c79f2bbc6bfc15b7e898a35ec094
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-05 00:17:36 +0600

    Refactor main function to support desktop and wasm32 configurations

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 mod components;
-mod task_manager;
+mod models;
 
+use components::TaskManager;
 use dioxus::prelude::*;
-use task_manager::TaskManager;
 
 // const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
-    launch(App);
-}
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        use dioxus::desktop;
+        let window_builder = desktop::WindowBuilder::new()
+            .with_always_on_top(true)
+            .with_title("Task Manager");
 
+        dioxus::LaunchBuilder::new()
+            .with_cfg(
+                desktop::Config::default()
+                    .with_menu(None)
+                    .with_window(window_builder),
+            )
+            .launch(App);
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        launch(App);
+    }
+}
 
 #[component]
 fn App() -> Element {
