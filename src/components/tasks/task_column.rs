5bff2cfc9a1c3d08cf5b8a730b2ffaa7a633f568
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-06 00:37:12 +0600

    Refactor TaskColumn component to use task_list for task retrieval and improve variable naming for clarity

--- src/components/tasks/task_column.rs
+++ src/components/tasks/task_column.rs
@@ -2,20 +2,20 @@ use dioxus::prelude::*;
 
 use crate::{components::TaskCard, models::TaskGroup};
 
-
 #[component]
 pub fn TaskColumn(task_group: TaskGroup) -> Element {
     let name = task_group.name;
     let description = task_group.description.as_deref().unwrap_or("");
-    
+    let task_list = task_group.task_list.read();
+
     rsx! {
         div { class: "bg-stone-100 my-2 p-4 rounded",
             div { class: "bg-stone-200 p-4 rounded",
                 h3 { "{name}" }
                 p { "{description}" }
             }
-            for task in task_group.task_list.iter() {
-                TaskCard { task: task.clone() }
+            for task_state in task_list.iter() {
+                TaskCard { task_state: task_state.clone() }
             }
         }
     }
