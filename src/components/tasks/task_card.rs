0b8da709ee43f8169eb9ed3e991a006fffce8551
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-06 00:37:07 +0600

    Refactor TaskCard component to use task_state for title retrieval and remove unused description and priority display

--- src/components/tasks/task_card.rs
+++ src/components/tasks/task_card.rs
@@ -3,20 +3,13 @@ use dioxus::prelude::*;
 use crate::models::Task;
 
 #[component]
-pub fn TaskCard(task: Task) -> Element {
-    let title = task.title;
-    let description = task.description.read().as_deref().unwrap_or("").to_string();
+pub fn TaskCard(task_state: Task) -> Element {
+    let title = &task_state.task.read().title;
+    // let description = task_.description.read().as_deref().unwrap_or("").to_string();
 
     rsx! {
         div { class: "bg-white my-2 p-4 rounded shadow",
             h3 { "{title}" }
-            span {
-                class: "font-bold text-sm border-gray-500 border-3 p-1 rounded",
-                onclick: move |_| {
-                    task.change_priority();
-                },
-                "{task.priority}"
-            }
         }
     }
 }
