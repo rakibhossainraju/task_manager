8606d3250a6a36c8c7ff8c8498d7f566e4aa873f
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-06 15:15:27 +0600

    Refactor TaskColumn component to improve readability and simplify task group handling

--- src/components/tasks/task_column.rs
+++ src/components/tasks/task_column.rs
@@ -4,25 +4,25 @@ use crate::{components::TaskCard, models::TaskGroup};
 
 #[component]
 pub fn TaskColumn(task_group: Signal<TaskGroup>) -> Element {
-    let task_group_read = task_group.read();
-    let title = task_group_read.title.as_str();
-    let description = task_group_read.description.as_deref().unwrap_or("");
-    let change_group_title = {
-        let mut task_group = task_group.clone();
-        move |_| {
-            task_group.write().update_group_title("New Title!");
-        }
+    let group = task_group.read();
+
+    let title = &group.title;
+    let description = group.description.as_deref().unwrap_or("");
+
+    let change_group_title = move |_| {
+        task_group.write().update_group_title("New Title!");
     };
+
     info!("Re-Rendierng-column");
 
     rsx! {
         div { class: "bg-stone-100 my-2 p-4 rounded",
             div { class: "bg-stone-200 p-4 rounded",
-                h3 {class: "font-medium text-lg", onclick: change_group_title, "{title}" }
+                h3 { class: "font-medium text-lg", onclick: change_group_title, "{title}" }
                 p { "{description}" }
             }
-            for task in task_group_read.task_list.iter() {
-                TaskCard { task_signal: task.task, key: "{task.id}" }
+            for task_signal in group.task_list.iter() {
+                TaskCard { task_signal: *task_signal, key: "{task_signal.read().id}" }
             }
         }
     }
