4407d947696c5b8ba22a5a8073862657ad7d34e2
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-06 14:25:15 +0600

    Refactor TaskCard component to accept Signal<TaskData>, update title on click, and add priority toggle functionality

--- src/components/tasks/task_card.rs
+++ src/components/tasks/task_card.rs
@@ -1,23 +1,30 @@
 use dioxus::prelude::*;
 
-use crate::models::Task;
+use crate::models::{TaskData};
 
 #[component]
-pub fn TaskCard(task_state: Task) -> Element {
-    let task_signal = task_state.task;
+pub fn TaskCard(task_signal: Signal<TaskData>) -> Element {
     let task_read = task_signal.read();
     let title = task_read.title.as_str();
+    let priority = &task_signal.read().priority;
 
-    let mut task_signal = task_state.task.clone();
     let handle_click = move |_| {
         task_signal
             .write()
             .update_title("You have changed the title!");
     };
-    info!("Rendering TaskCard with title: {}", title);
+    let change_priority = move |_| {
+      task_signal.write().toggle_priority();
+    };
+    info!("Re-Rendierng-card");
     rsx! {
         div { class: "bg-white my-2 p-4 rounded shadow",
             h3 { onclick: handle_click, "{title}" }
+            button {
+                class: "rounded border-2 border-gray-500 px-1 py-0.5 text-xs font-medium cursor-pointer",
+                onclick: change_priority,
+                "{priority}"
+            }
         }
     }
 }
