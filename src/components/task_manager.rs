9227fccaccd549fcca3117e5f568c5d1fec25b5d
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-06 14:25:42 +0600

    Refactor task group addition to use a test group name and improve state management

--- src/components/task_manager.rs
+++ src/components/task_manager.rs
@@ -7,13 +7,14 @@ pub fn TaskManager() -> Element {
     let task_groups = use_signal(TasksGroupsState::new);
 
     let add_task_group = {
-        let mut task_groups = task_groups.clone();
         move |_| {
-            // task_groups.with_mut(|tg| {
-            //     tg.add_task_group("Todo".to_string());
-            // });
+            let mut task_groups = task_groups.clone();
+            task_groups.with_mut(|tg| {
+               tg.add_task_group("Test Group");
+            });
         }
     };
+    info!("Re-Rendierng-The APP");
     rsx! {
         h1 { class: "font-extrabold text-2xl mb-4", "Task Manager" }
         button {
