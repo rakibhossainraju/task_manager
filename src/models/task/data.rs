446f718be837e6bc12f75974cb900180cb4d36d5
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-06 15:00:48 +0600

    Refactor Task struct to include title, description, priority, and timestamps; reintroduce update_title and toggle_priority methods

--- src/models/task/data.rs
+++ src/models/task/data.rs
@@ -29,15 +29,3 @@ impl TaskData {
         task
     }
 }
-
-impl TaskData {
-    pub fn update_title(&mut self, new_title: impl Into<String>) {
-        self.title = new_title.into();
-        self.updated_at = Some(chrono::Local::now());
-    }
-
-    pub fn toggle_priority(&mut self) {
-        self.priority = self.priority.toggle_priority();
-        self.updated_at = Some(Local::now());
-    }
-}
--- src/models/task/mod.rs
+++ src/models/task/mod.rs
@@ -1,15 +1,21 @@
 mod data;
 mod priority;
 
+use chrono::{DateTime, Local};
 use dioxus::prelude::*;
 use uuid::Uuid;
 
 pub use crate::models::task::data::TaskData;
+use crate::models::task::priority::TaskPriority;
 
 #[derive(Debug, Props, Clone, PartialEq)]
 pub struct Task {
     pub id: Uuid,
-    pub task: Signal<TaskData>,
+    pub title: String,
+    pub description: Option<String>,
+    pub priority: TaskPriority,
+    pub created_at: DateTime<Local>,
+    pub updated_at: Option<DateTime<Local>>,
 }
 
 impl From<TaskData> for Task {
@@ -22,7 +28,20 @@ impl Task {
     pub fn new(data: TaskData) -> Self {
         Self {
             id: data.id,
-            task: Signal::new(data),
+            title: data.title,
+            description: data.description,
+            priority: data.priority,
+            created_at: data.created_at,
+            updated_at: data.updated_at,
         }
+    }
+    pub fn update_title(&mut self, new_title: impl Into<String>) {
+        self.title = new_title.into();
+        self.updated_at = Some(Local::now());
+    }
+
+    pub fn toggle_priority(&mut self) {
+        self.priority = self.priority.toggle_priority();
+        self.updated_at = Some(Local::now());
     }
 }
