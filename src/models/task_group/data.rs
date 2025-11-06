8e0d489de4baed338f529cf5efbea46c866b6b6d
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-06 14:23:31 +0600

    Rename 'name' to 'title' in TaskGroupData and implement a new constructor for initialization

--- src/models/task_group/data.rs
+++ src/models/task_group/data.rs
@@ -6,7 +6,19 @@ use crate::models::TaskData;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TaskGroupData {
     pub id: Uuid,
-    pub name: String,
+    pub title: String,
     pub description: Option<String>,
     pub task_list: Vec<TaskData>,
+}
+
+impl TaskGroupData {
+    pub fn new(title: String, description: Option<String>, task_list: Option<Vec<TaskData>>) -> Self {
+        let description = description.map(|d| d.into());
+        Self {
+            id: Uuid::new_v4(),
+            title,
+            description,
+            task_list: task_list.unwrap_or(Vec::new()),
+        }
+    }
 }
