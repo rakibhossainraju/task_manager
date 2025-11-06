58425d8b46bb6778f98528feb4b1dc4735fdc0c4
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-06 14:25:29 +0600

    Refactor TaskColumn component to accept Signal<TaskGroup>, update title on click, and improve task rendering

--- src/components/tasks/task_column.rs
+++ src/components/tasks/task_column.rs
@@ -3,20 +3,28 @@ use dioxus::prelude::*;
 use crate::{components::TaskCard, models::TaskGroup};
 
 #[component]
-pub fn TaskColumn(task_group: TaskGroup) -> Element {
-    let name = task_group.name;
-    let description = task_group.description.as_deref().unwrap_or("");
-    let task_list = task_group.task_list.read();
+pub fn TaskColumn(task_group: Signal<TaskGroup>) -> Element {
+    let task_group_read = task_group.read();
+    let title = task_group_read.title.as_str();
+    let description = task_group_read.description.as_deref().unwrap_or("");
+    let change_group_title = {
+        let mut task_group = task_group.clone();
+        move |_| {
+            task_group.write().update_group_title("New Title!");
+        }
+    };
+    info!("Re-Rendierng-column");
 
     rsx! {
         div { class: "bg-stone-100 my-2 p-4 rounded",
             div { class: "bg-stone-200 p-4 rounded",
-                h3 { "{name}" }
+                h3 {class: "font-medium text-lg", onclick: change_group_title, "{title}" }
                 p { "{description}" }
             }
-            for task_state in task_list.iter() {
-                TaskCard { task_state: task_state.clone() }
+            for task in task_group_read.task_list.iter() {
+                TaskCard { task_signal: task.task, key: "{task.id}" }
             }
         }
     }
 }
+
