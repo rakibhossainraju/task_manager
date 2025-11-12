7060b67cb312494565210bb1518ae434499decf0
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-12 09:22:49 +0600

    Refactor Input component for improved layout and input handling

--- src/components/filter/input.rs
+++ src/components/filter/input.rs
@@ -1,19 +1,34 @@
-use dioxus::prelude::*;
+use dioxus::{html::input, prelude::*};
 
 const SEARCH_ICON: &str = include_str!("../../../assets/icons/search-icon.svg");
 
 #[component]
 pub fn Input() -> Element {
+    let mut input_value = use_signal(String::new);
+
     rsx! {
-        div { class: "flex items-center gap-4 px-4 py-2 justify-start",
-            span { class: "", dangerous_inner_html: SEARCH_ICON }
-            div { class: "relative w-full",
+        div { class: "flex items-center justify-start gap-2.5 px-4 bg-primary drop-shadow-default rounded border border-transparent focus-within:border-slate-300",
+            span { dangerous_inner_html: SEARCH_ICON }
+            div { class: "relative w-full text-slate-400 text-lg",
                 input {
-                    class: "px-4 py-3 text-lg",
+                    class: "w-full  py-4 text-lg outline-none border-none",
                     r#type: "text",
-                    placeholder: "",
+                    oninput: move |e| {
+                        input_value.set(e.data.value());
+                    },
                 }
-                span { class: "absolute top-1/2 -translate-y-1/2 block text-slate-400 text-lg",
+                span {
+                    class: {
+                        let visibility_class: &str = if input_value.read().is_empty() {
+                            "visible"
+                        } else {
+                            "invisible"
+                        };
+                        format!(
+                            "absolute top-1/2 -translate-y-1/2 block pointer-events-none {}",
+                            visibility_class,
+                        )
+                    },
                     "Filter by "
                     b { class: "font-fira-mono font-medium", "priority" }
                     ", "
