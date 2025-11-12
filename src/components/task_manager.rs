e4669778954624b00b9c25ba2b79c03041a71bc3
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-12 09:22:37 +0600

    Remove commented-out code from TaskManager component for clarity

--- src/components/task_manager.rs
+++ src/components/task_manager.rs
@@ -10,25 +10,25 @@ pub fn TaskManager() -> Element {
     let task_groups = use_signal(TasksGroupsState::new);
 
     let add_task_group = {
-        move |_| {
-            let mut task_groups = task_groups.clone();
-            task_groups.with_mut(|tg| {
-                tg.add_task_group("Test Group");
-            });
-        }
+        // move |_| {
+        //     let mut task_groups = task_groups.clone();
+        //     task_groups.with_mut(|tg| {
+        //         tg.add_task_group("Test Group");
+        //     });
+        // }
     };
     info!("Re-Rendierng-The APP");
     rsx! {
         Filter {}
-        button {
-            class: "bg-blue-500 text-white px-4 py-2 rounded mb-4",
-            onclick: add_task_group,
-            "+ Add Task Group"
-        }
+        // button {
+        //     class: "bg-blue-500 text-white px-4 py-2 rounded mb-4",
+        //     onclick: add_task_group,
+        //     "+ Add Task Group"
+        // }
         div {
-            for group in task_groups.read().groups.iter() {
-                TaskColumn { task_group: group.clone() }
-            }
+            // for group in task_groups.read().groups.iter() {
+            //     TaskColumn { task_group: group.clone() }
+            // }
         }
     }
 }
