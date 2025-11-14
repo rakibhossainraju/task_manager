bde9feb9781eadf604ddf7e19f2d7503cecbf2fb
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-14 23:41:38 +0600

    Refactor Input component for consistent padding and improved layout

--- src/components/filter/input.rs
+++ src/components/filter/input.rs
@@ -1,4 +1,4 @@
-use dioxus::{html::input, prelude::*};
+use dioxus::prelude::*;
 
 const SEARCH_ICON: &str = include_str!("../../../assets/icons/search-icon.svg");
 
@@ -7,11 +7,11 @@ pub fn Input() -> Element {
     let mut input_value = use_signal(String::new);
 
     rsx! {
-        div { class: "flex items-center justify-start gap-2.5 px-4 bg-primary drop-shadow-default rounded border border-transparent focus-within:border-slate-300",
+        div { class: "flex items-center justify-start gap-2.5 px-4.5 bg-primary drop-shadow-default rounded border border-transparent focus-within:border-slate-300 w-full",
             span { dangerous_inner_html: SEARCH_ICON }
             div { class: "relative w-full text-slate-400 text-lg",
                 input {
-                    class: "w-full  py-4 text-lg outline-none border-none",
+                    class: "w-full  py-2.5 text-lg outline-none border-none",
                     r#type: "text",
                     oninput: move |e| {
                         input_value.set(e.data.value());
