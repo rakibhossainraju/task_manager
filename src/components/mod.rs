211b3de35f9f58b81436d840cbaa8407f741b60e
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-15 12:57:35 +0600

    Add task_manager_provider module to mod.rs for improved context management

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,7 +1,9 @@
 mod filter;
 mod task_manager;
+mod task_manager_provider;
 mod tasks;
 
 pub use filter::Filter;
 pub use task_manager::*;
+pub use task_manager_provider::TaskManagerProvider;
 pub use tasks::*;
