c2de9cb2529c0112353920d1010f9f8082b699bc
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-15 12:59:21 +0600

    Refactor Button component to improve onclick handling and formatting

--- src/components/filter/button.rs
+++ src/components/filter/button.rs
@@ -20,14 +20,22 @@ impl FilterButtonType {
 #[derive(Clone, PartialEq, Props)]
 pub struct ButtonProps {
     pub button_type: FilterButtonType,
+    onclick: Option<EventHandler<MouseEvent>>,
     children: Element,
 }
 
 #[component]
 pub fn Button(props: ButtonProps) -> Element {
     let button_class = props.button_type.class();
+    let onclick = move |e| {
+        if let Some(handler) = &props.onclick {
+            handler.call(e);
+        }
+    };
     rsx! {
-        button { class: "text-white font-medium rounded drop-shadow-default px-4.5 py-3 cursor-pointer {button_class}",
+        button {
+            class: "text-white font-medium rounded drop-shadow-default px-4.5 py-3 cursor-pointer {button_class}",
+            onclick,
             {props.children}
         }
     }
