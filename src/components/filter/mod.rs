71450ebb8498fd91677c88b8b1866721b9f2afc5
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-15 12:59:49 +0600

    Refactor Filter component to improve data flow and add task group functionality

--- src/components/filter/mod.rs
+++ src/components/filter/mod.rs
@@ -4,7 +4,10 @@ mod input;
 use dioxus::prelude::*;
 use input::Input;
 
-use crate::components::filter::button::{Button, FilterButtonType};
+use crate::{
+    components::filter::button::{Button, FilterButtonType},
+    models::{add_task_group, use_task_manager},
+};
 
 const COLUMN_SVG: &str = include_str!("../../../assets/icons/column.svg");
 const LIST_SVG: &str = include_str!("../../../assets/icons/list.svg");
@@ -12,6 +15,13 @@ const PLUS_SVG: &str = include_str!("../../../assets/icons/plus.svg");
 
 #[component]
 pub fn Filter() -> Element {
+    let ctx = use_task_manager();
+    let mut task_groups = ctx.task_groups;
+    let handle_add_task_group = move |_| {
+        task_groups.with_mut(|state| {
+            add_task_group(state, "Test Group");
+        });
+    };
     rsx! {
         div { class: "flex w-full gap-3.5",
             Input {}
@@ -28,7 +38,9 @@ pub fn Filter() -> Element {
                 Button { button_type: FilterButtonType::Outline,
                     span { dangerous_inner_html: LIST_SVG }
                 }
-                Button { button_type: FilterButtonType::Outline,
+                Button {
+                    button_type: FilterButtonType::Outline,
+                    onclick: handle_add_task_group,
                     div { class: "flex items-center justify-center gap-2 min-w-[155px]",
                         span { dangerous_inner_html: PLUS_SVG }
                         span { class: "text-slate-400", "Add New Column" }
