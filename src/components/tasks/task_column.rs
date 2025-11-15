165c73cf60e2ab0f3750a96210ae4e745ac15718
rakibhossainraju <rakibhossianraju@gmail.com>
2025-11-15 13:00:20 +0600

    Refactor TaskColumn component to improve data flow by removing unused event handlers and utilizing task manager context for group title updates

--- src/components/tasks/task_column.rs
+++ src/components/tasks/task_column.rs
@@ -1,43 +1,39 @@
 use dioxus::prelude::*;
 
-use crate::{components::TaskCard, models::TaskGroup};
+use crate::{
+    components::TaskCard,
+    models::{TaskGroup, update_group, use_task_manager},
+};
 
 #[derive(Props, Clone, PartialEq)]
 pub struct TaskColumnProps {
     group: TaskGroup,
-    #[props(default)]
-    on_update_title: Option<EventHandler<(uuid::Uuid, String)>>,
-    #[props(default)]
-    on_task_title_change: Option<EventHandler<(uuid::Uuid, uuid::Uuid, String)>>,
-    #[props(default)]
-    on_toggle_priority: Option<EventHandler<(uuid::Uuid, uuid::Uuid)>>,
 }
 
 #[component]
 pub fn TaskColumn(props: TaskColumnProps) -> Element {
+    let ctx = use_task_manager();
+    let mut task_groups = ctx.task_groups;
     let title = &props.group.title;
     let description = props.group.description.as_deref().unwrap_or("");
+    let group_id = props.group.id;
 
     let change_group_title = move |_| {
-        if let Some(handler) = &props.on_update_title {
-            handler.call((props.group.id, "New Title!".to_string()));
-        }
+        task_groups.with_mut(|state| {
+            update_group(state, group_id, |g| {
+                g.update_group_title("New Title!".to_string());
+            });
+        });
     };
 
     rsx! {
-        div { class: "bg-stone-100 my-2 p-4 rounded min-w-[300px]",
+        div { class: "bg-stone-100 p-4 rounded min-w-[300px]",
             div { class: "bg-stone-200 p-4 rounded",
                 h3 { class: "font-medium text-lg", onclick: change_group_title, "{title}" }
                 p { "{description}" }
             }
             for task in props.group.task_list.iter() {
-                TaskCard { 
-                    task: task.clone(),
-                    group_id: props.group.id,
-                    on_title_change: props.on_task_title_change,
-                    on_toggle_priority: props.on_toggle_priority,
-                    key: "{task.id}"
-                }
+                TaskCard { task: task.clone(), group_id, key: "{task.id}" }
             }
         }
     }
